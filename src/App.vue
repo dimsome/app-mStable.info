4f5e2bf9bd5ea9adf2a86d9f0e6617ed1c26de93
dimsomedim <dimitri@golecko.de>
2021-06-18 01:29:19 +0700

    Added Frontend - working table

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>mStable - Voting Participation</h1>
+  <div v-if="error">{{ error }}</div>
+  <div v-else>
+    <div v-if="data.length">
+      <Table :data="data" />
+    </div>
+    <div v-else>Loading</div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Table from "./components/Table.vue";
+import { ref } from "vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Table,
+  },
+  setup() {
+    const data = ref([]);
+    const error = ref(null);
+
+    const fetchData = async () => {
+      try {
+        let response = await fetch("http://127.0.0.1:4000/api/proposals");
+        // console.log(response);
+        if (!response.ok) throw Error("Data not fetched");
+        data.value = await response.json();
+      } catch (err) {
+        error.value = err.message;
+      }
+    };
+
+    fetchData();
+
+    return { data, error };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+body {
+  font-family: Poppins, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: rgb(37, 39, 45);
   margin-top: 60px;
+  background: #f8f5f2;
 }
 </style>
---
+++ src/components/Table.vue
@@ -0,0 +1,77 @@
+<template>
+  <h2>hello</h2>
+  <div v-if="!tableData.length">Table is empty</div>
+  <div v-else>
+    <table class="styled-table">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Start</th>
+          <th>End</th>
+          <th>Num Voters</th>
+          <th>Unique Holders</th>
+          <th>vMTA voted</th>
+          <th>vMTA total</th>
+          <th>State</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="data in tableData" :key="data._id">
+          <td>{{ data.title }}</td>
+          <td>{{ data.start }}</td>
+          <td>{{ data.end }}</td>
+          <td>{{ data.votersNum }}</td>
+          <td>{{ data.votersTotal }}</td>
+          <td>{{ data.scoreNum }}</td>
+          <td>{{ data.scoreTotal }}</td>
+          <td>{{ data.state }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Table",
+  props: ["data"],
+  setup(props) {
+    console.log(props.data[0].keys);
+
+    return { tableData: props.data };
+  },
+};
+</script>
+
+<style>
+.styled-table {
+  border-collapse: collapse;
+  margin: 25px 0;
+  font-size: 0.9em;
+  font-family: sans-serif;
+  min-width: 400px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+}
+.styled-table thead tr {
+  background-color: #009879;
+  color: #ffffff;
+  text-align: left;
+}
+.styled-table th,
+.styled-table td {
+  padding: 12px 15px;
+}
+.styled-table tbody tr {
+  border-bottom: 1px solid #dddddd;
+}
+.styled-table tbody tr:nth-of-type(even) {
+  background-color: #f3f3f3;
+}
+.styled-table tbody tr:last-of-type {
+  border-bottom: 2px solid #009879;
+}
+.styled-table tbody tr.active-row {
+  font-weight: bold;
+  color: #009879;
+}
+</style>
